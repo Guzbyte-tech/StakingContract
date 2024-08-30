dd6afa460177e2ce3739cca52547c7342f16d800
Ezeozue Chiagoziem <ezeozuechiagoziem@gmail.com>
2024-08-30 09:39:06 +0100

    Stake function added

--- contracts/StakeEther.sol
+++ contracts/StakeEther.sol
@@ -66,6 +66,35 @@ contract StakeEther {
         emit createPlanSuccessful(_nameOfPlan, _duration, _interestRate, _planId);
     }
 
+    function getAllStakingPlan() external view onlyOwner returns (stakingPlan[] memory) {
+        return allPlans;
+    }
+
+    //Function for user to stake to a plan in the contract
+    //User choose the plan they want to stake in
+    //Function is payable so we can recieve ether through this function
+     function stake(uint8 _planID) external payable {
+        require(msg.sender != address(0), "Sender address is a zero address");
+        require(msg.value > 0, "Amount to deposit must be greater than zero.");
+        require(plans[_planID].planId == _planID, "Invalid plan ID");
+        //Increase staked balance;
+        totalAmountStakedBalances[msg.sender] += msg.value;
+        //Get the plan User wants to stake in
+        stakingPlan memory selectedPlan = plans[_planID];
+        uint256 interest = calculateInterest(msg.value, selectedPlan.interestRate, selectedPlan.duration);
+        stakesByUser[msg.sender][_planID].push(userStake({
+            planId: _planID,
+            endTime: block.timestamp + (selectedPlan.duration * 24 * 60 * 60), //Convert the plan to seconds.
+            created_at: block.timestamp,
+            estimatedInterest: interest,
+            amountStaked: msg.value,
+            isEnded: false,
+            isWithdrawn: false
+        }));
+        
+        emit depositSuccessful(msg.value, msg.sender, selectedPlan.planName);
+    }
+
 
      modifier onlyOwner {
         require(msg.sender == owner, "You are not the owner of this contract");
