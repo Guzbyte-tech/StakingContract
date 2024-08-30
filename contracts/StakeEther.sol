685166c34a18e7a48ae33f03f148ff58a300e990
Ezeozue Chiagoziem <ezeozuechiagoziem@gmail.com>
2024-08-30 17:12:25 +0100

    Reward Mechanism Function and Calculate function added

--- contracts/StakeEther.sol
+++ contracts/StakeEther.sol
@@ -2,13 +2,11 @@
 pragma solidity ^0.8.17;
 
 contract StakeEther {
-
     address owner;
     uint256 daysInYear;
     uint256 public unlockTime;
 
-
-    constructor () {
+    constructor() {
         owner = msg.sender;
         daysInYear = 365;
         unlockTime = block.timestamp + 10 days;
@@ -40,21 +38,34 @@ contract StakeEther {
     //Map a plan to user
     // mapping (address => userStake[]) stakesByAddess;
 
-    mapping (address => mapping (uint8 => userStake[])) stakesByUser;
+    mapping(address => mapping(uint8 => userStake[])) stakesByUser;
     //Implement 2D Mapping
 
-    
-    stakingPlan [] allPlans;
+    stakingPlan[] allPlans;
 
-    event createPlanSuccessful(string nameOfPlan, uint256 _duration, uint256 interestRate, uint8 planId);
+    event createPlanSuccessful(
+        string nameOfPlan,
+        uint256 _duration,
+        uint256 interestRate,
+        uint8 planId
+    );
     event depositSuccessful(uint _amount, address _address, string planNmae);
 
     //Function to create staking plans
-    function createPlan(string memory _nameOfPlan, uint256 _duration, uint256 _interestRate, uint8 _planId) external onlyOwner  {
+    function createPlan(
+        string memory _nameOfPlan,
+        uint256 _duration,
+        uint256 _interestRate,
+        uint8 _planId
+    ) external onlyOwner {
         //check if planId already exists.
         bytes memory nameInByte = bytes(_nameOfPlan);
         require(!plans[_planId].exists, "Plan ID already Exists");
-        require(keccak256(abi.encodePacked(plans[_planId].planName)) != keccak256(abi.encodePacked(nameInByte)) , "Plan Name already Exists.");
+        require(
+            keccak256(abi.encodePacked(plans[_planId].planName)) ==
+                keccak256(abi.encodePacked(nameInByte)),
+            "Plan Name already Exists."
+        );
         stakingPlan memory sp;
         sp.planName = _nameOfPlan;
         sp.interestRate = _interestRate;
@@ -63,17 +74,27 @@ contract StakeEther {
         sp.exists = true;
         allPlans.push(sp);
         plans[_planId] = sp;
-        emit createPlanSuccessful(_nameOfPlan, _duration, _interestRate, _planId);
+        emit createPlanSuccessful(
+            _nameOfPlan,
+            _duration,
+            _interestRate,
+            _planId
+        );
     }
 
-    function getAllStakingPlan() external view onlyOwner returns (stakingPlan[] memory) {
+    function getAllStakingPlans()
+        external
+        view
+        onlyOwner
+        returns (stakingPlan[] memory)
+    {
         return allPlans;
     }
 
     //Function for user to stake to a plan in the contract
     //User choose the plan they want to stake in
     //Function is payable so we can recieve ether through this function
-     function stake(uint8 _planID) external payable {
+    function stake(uint8 _planID) external payable {
         require(msg.sender != address(0), "Sender address is a zero address");
         require(msg.value > 0, "Amount to deposit must be greater than zero.");
         require(plans[_planID].planId == _planID, "Invalid plan ID");
@@ -81,25 +102,76 @@ contract StakeEther {
         totalAmountStakedBalances[msg.sender] += msg.value;
         //Get the plan User wants to stake in
         stakingPlan memory selectedPlan = plans[_planID];
-        uint256 interest = calculateInterest(msg.value, selectedPlan.interestRate, selectedPlan.duration);
-        stakesByUser[msg.sender][_planID].push(userStake({
-            planId: _planID,
-            endTime: block.timestamp + (selectedPlan.duration * 24 * 60 * 60), //Convert the plan to seconds.
-            created_at: block.timestamp,
-            estimatedInterest: interest,
-            amountStaked: msg.value,
-            isEnded: false,
-            isWithdrawn: false
-        }));
-        
+        uint256 interest = calculateInterest(
+            msg.value,
+            selectedPlan.interestRate,
+            selectedPlan.duration
+        );
+        stakesByUser[msg.sender][_planID].push(
+            userStake({
+                planId: _planID,
+                endTime: block.timestamp +
+                    (selectedPlan.duration * 24 * 60 * 60), //Convert the plan to seconds.
+                created_at: block.timestamp,
+                estimatedInterest: interest,
+                amountStaked: msg.value,
+                isEnded: false,
+                isWithdrawn: false
+            })
+        );
+
         emit depositSuccessful(msg.value, msg.sender, selectedPlan.planName);
     }
 
+    function rewardMechanism(uint8 _planID, uint256 _index) external {
+        require(msg.sender != address(0), "Address zero Detected");
+        require(
+            totalAmountStakedBalances[msg.sender] > 0,
+            "Not a user of the ystem"
+        );
+        require(
+            canWithdraw(_planID, msg.sender, _index),
+            "Cannot perform reward."
+        );
+        userStake storage usrStk = stakesByUser[msg.sender][_planID][_index];
+        usrStk.isEnded = true;
+        usrStk.isWithdrawn = true;
+        uint256 interest = usrStk.estimatedInterest;
+        rewardBalances[msg.sender] += interest;
+        totalAmountRewardedBalances[msg.sender] += interest;
+    }
 
-     modifier onlyOwner {
+    //Withdraw money if plan set has elapsed.
+
+    function withdrawReward(uint _amount) external {
+        require(msg.sender != address(0), "Address zero detected");
+        require(
+            rewardBalances[msg.sender] >= _amount,
+            "Insufficient funds in reward balance."
+        );
+        require(
+            address(this).balance >= _amount,
+            "Contract balance is insufficient."
+        );
+        uint256 bal = rewardBalances[msg.sender];
+        uint newBal = bal - _amount;
+        rewardBalances[msg.sender] = newBal;
+        (bool success, ) = msg.sender.call{value: _amount}("");
+        require(success, "Withdrawal Failed!");
+    }
+
+    modifier onlyOwner() {
         require(msg.sender == owner, "You are not the owner of this contract");
         _;
     }
 
-
+    function calculateInterest(
+        uint256 principal,
+        uint256 rate,
+        uint numberOfDays
+    ) public view returns (uint256) {
+        uint256 timeInYears = (numberOfDays * 1e18) / daysInYear; // Time in years scaled by 1e18 for precision
+        uint256 interest = (principal * rate * timeInYears) / 100e18; // Calculate interest with scaling
+        return principal + interest;
+    }
 }
